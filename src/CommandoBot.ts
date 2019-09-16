f3349a15e01b0d58b0386232aa281347391e84be
FoxxMD <FoxxMD@users.noreply.github.com>
2019-09-16 14:13:06 -0400

    Cleanup comments

--- src/CommandoBot.ts
+++ src/CommandoBot.ts
@@ -1,7 +1,6 @@
-import {Bot, BotConstructorInterface, Environment, eventType} from "./Bot";
+import {Bot, BotConstructorInterface, Environment } from "./Bot";
 import {Database, open} from "sqlite";
 import {CommandoClient, CommandoClientOptions, SQLiteProvider} from 'discord.js-commando';
-//import {getUserFromMention} from "./utilities";
 
 export interface CommandoBotConstructorInterface extends BotConstructorInterface {
     commando?: {
@@ -12,7 +11,6 @@ export interface CommandoBotConstructorInterface extends BotConstructorInterface
 
 export interface CommandEnvironment extends Environment {
     commando?: {
-        // allowMentionPrefix?: boolean
         clientOptions?: CommandoClientOptions
     }
 
@@ -22,7 +20,6 @@ export class CommandoBot extends Bot {
 
     commandoDb!: Database;
     client: CommandoClient;
-    //allowMentionPrefix: boolean;
 
     constructor(props: CommandoBotConstructorInterface) {
         super(props);
@@ -33,35 +30,17 @@ export class CommandoBot extends Bot {
             } = {},
             env: {
                 commando: {
-                    //allowMentionPrefix = true,
                     clientOptions = undefined
                 } = {}
             }
         } = props;
 
-        //this.allowMentionPrefix = allowMentionPrefix;
 
         this.client = new CommandoClient(clientOptions);
 
         if (commandoDb !== undefined) {
             this.commandoDb = commandoDb;
         }
-
-        // this.events.ready.push({
-        //     type: eventType.BOT_PRE,
-        //     runOnce: true,
-        //     func: async() => {
-        //         if(this.allowMentionPrefix === false) {
-        //             this.client.dispatcher.addInhibitor(msg => {
-        //                 const {content} = msg;
-        //                 if (content.startsWith(this.client.commandPrefix)) return false;
-        //                 const mentionMatch = getUserFromMention(content.split(/ +/)[0], msg.mentions.users);
-        //                 return mentionMatch ? 'no'
-        //                 // this.client.user.id
-        //             })
-        //         }
-        //     }
-        // })
     }
 
     protected initializeDB = async (): Promise<void> => {
